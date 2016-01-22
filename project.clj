91dbdd07e0ee4bf26eac102f8114852f4ba7d2e0
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2016-01-21 18:22:25 -0800

    project update

--- project.clj
+++ project.clj
@@ -1,8 +1,4 @@
-(defproject back "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject back "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [http-kit "2.1.18"]
                  [polaris "0.0.15"]
@@ -11,5 +7,5 @@
                  [clj-tagsoup "0.3.0"]
                  [org.clojure/data.json "0.2.6"]                 
                  [org.clojure/math.numeric-tower "0.0.4"]]
-  
+  :min-lein-version "2.0.0"  
   :main back.server)
